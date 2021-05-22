93837ff939e8549710965326386d898dda64cc99
Erik Kühlmann Salazar <ekuhlmann23@gmail.com>
2021-05-22 13:35:30 -0600

    Initialize firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:escuela_verano/pages/counter_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(ProviderScope(
     child: MyApp(),
   ));
